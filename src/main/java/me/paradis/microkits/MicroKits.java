ffc0a9113d8c05294505dd66c68ebed623beeac4
Paradis <Lucasezequiel120202@gmail.com>
2022-04-16 20:02:24 -0300

    basic setup

--- src/main/java/me/paradis/microkits/MicroKits.java
+++ src/main/java/me/paradis/microkits/MicroKits.java
@@ -1,17 +1,34 @@
 package me.paradis.microkits;
 
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MicroKits extends JavaPlugin {
 
+    public static MicroKits instance;
+
+    FileConfiguration config = getConfig();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+        saveDefaultConfig();
 
+        getServer().getPluginManager().registerEvents(new RightClickPaperManager(), this);
+
+        this.getCommand("microkits").setExecutor(new CommandManager());
+
+        getLogger().info("MicroKits Enabled - Made by Paradis");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        saveConfig();
+    }
+
+    public static MicroKits getInstance() {
+        return instance;
     }
 }
---
+++ src/main/java/me/paradis/microkits/RightClickPaperManager.java
@@ -0,0 +1,6 @@
+package me.paradis.microkits;
+
+import org.bukkit.event.Listener;
+
+public class RightClickPaperManager implements Listener {
+}
